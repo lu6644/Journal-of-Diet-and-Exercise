b2a8391d4186a500b8365793a21762df5df861ea
Huiguang <109044385+JamesJin0519@users.noreply.github.com>
2023-10-17 00:27:08 -0400

    Update NutrientIntakeTracker.java

--- src/main/java/Model/DataProcessing/NutrientIntakeTracker.java
+++ src/main/java/Model/DataProcessing/NutrientIntakeTracker.java
@@ -1,4 +1,4 @@
-package Project;
+package Model.DataProcessing;
 
 import java.util.Scanner;
 
@@ -11,35 +11,25 @@ public class NutrientIntakeTracker {
         System.out.print("Enter the number of days to track nutrient intake: ");
         int numberOfDays = scanner.nextInt();
 
-        double totalProtein = 0;
-        double totalCarbohydrates = 0;
-        double totalOtherNutrients = 0;
+        NutrientTracker tracker = new NutrientTracker();
 
         for (int day = 1; day <= numberOfDays; day++) {
             System.out.println("\nDay " + day);
             System.out.print("Enter the amount of protein (grams) for day " + day + ": ");
             double protein = scanner.nextDouble();
-            totalProtein += protein;
 
             System.out.print("Enter the amount of carbohydrates (grams) for day " + day + ": ");
             double carbohydrates = scanner.nextDouble();
-            totalCarbohydrates += carbohydrates;
 
             System.out.print("Enter the amount of other nutrients (grams) for day " + day + ": ");
             double otherNutrients = scanner.nextDouble();
-            totalOtherNutrients += otherNutrients;
+
+            DailyNutrientIntake dailyIntake = new DailyNutrientIntake(day, protein, carbohydrates, otherNutrients);
+            tracker.addDailyIntake(dailyIntake);
         }
 
-        // Calculate percentages
-        double totalNutrients = totalProtein + totalCarbohydrates + totalOtherNutrients;
-        double proteinPercentage = (totalProtein / totalNutrients) * 100;
-        double carbohydratesPercentage = (totalCarbohydrates / totalNutrients) * 100;
-        double otherNutrientsPercentage = (totalOtherNutrients / totalNutrients) * 100;
-
-        System.out.println("\nAverage Daily Nutrient Intake Over " + numberOfDays + " days:");
-        System.out.println("Protein: " + proteinPercentage + "%");
-        System.out.println("Carbohydrates: " + carbohydratesPercentage + "%");
-        System.out.println("Other Nutrients: " + otherNutrientsPercentage + "%");
+        tracker.calculatePercentages();
+        tracker.printSummary();
 
         scanner.close();
     }
