1fd3520f3e958763c99029c0dc814cd64cb76858
Huiguang <109044385+JamesJin0519@users.noreply.github.com>
2023-11-08 18:50:30 -0500

    Update Main.java

--- src/main/java/Model/DataProcessing/Main.java
+++ src/main/java/Model/DataProcessing/Main.java
@@ -3,6 +3,9 @@ package Model.DataProcessing;
 import Model.Diet.Diet;
 import Model.Diet.Food;
 import Model.Diet.MealType;
+import Model.DataProcessing.DailyNutrientIntake;
+import Model.DataProcessing.NutrientTracker;
+import Model.Diet.Nutrient;
 
 import java.util.Date;
 
@@ -18,14 +21,22 @@ public class Main {
         
         diet.addIngredient(chicken, 150.0);
         diet.addIngredient(rice, 300.0);
-
+        
+        DailyNutrientIntake intake1 = new DailyNutrientIntake(1, 50.0, 200.0, 30.0);
+        DailyNutrientIntake intake2 = new DailyNutrientIntake(2, 60.0, 180.0, 25.0);
+        NutrientTracker nutrientTracker = new NutrientTracker();
+        
+        nutrientTracker.addDailyIntake(intake1);
+        nutrientTracker.addDailyIntake(intake2);
+        
         
         System.out.println("Diet Information:");
         System.out.println("Date: " + diet.getDate());
         System.out.println("Meal Type: " + diet.getMeal());
         System.out.println("Ingredients: " + diet.getIngredients());
 
-       
+        nutrientTracker.printSummary();
+        nutrientTracker.calculatePercentages();
     }
 }
 
