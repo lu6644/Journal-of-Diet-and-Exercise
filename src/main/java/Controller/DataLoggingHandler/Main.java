e8e3259b2d03089fddc10f9e12677edfe7c68872
Lu Shen <853166476@qq.com>
2023-11-08 19:40:48 -0500

    update main for controller

--- src/main/java/Controller/DataLoggingHandler/Main.java
+++ src/main/java/Controller/DataLoggingHandler/Main.java
@@ -3,6 +3,10 @@ package Controller.DataLoggingHandler;
 import Controller.DataLoggingHandler.ExerciseLogging.BikingStrategy;
 import Model.Profile.UserProfile;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 	public static void main(String args[]) {
 		
@@ -18,6 +22,11 @@ public class Main {
 		System.out.println("The burnt calories of this exercise is: "+el.calcualteCalories()+" cals");
 		
 		//for Diet Logging Controller
-		
+		DietLoggingController dietLoggingController = DietLoggingController.getInstance();
+		Map<String, Double> foods = new HashMap<>();
+		foods.put("Butter, unsalted", 25.0);
+		foods.put("Beef, ground, regular", 150.0);
+		String nutrientsSummary = dietLoggingController.logDiet(1, new Date(123, 9, 29),"LUNCH",foods);
+		System.out.println(String.format("A Summary of Nutrients Intake for added meal is:\n%s", nutrientsSummary));
 	}
 }
