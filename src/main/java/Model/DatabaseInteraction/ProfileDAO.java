0654cc3da1c0ac464a1bd5f5ed847d921bc7029b
Lu Shen <853166476@qq.com>
2023-11-16 18:18:44 -0500

    existing profile selection MVC and DB Access

--- src/main/java/Model/DatabaseInteraction/ProfileDAO.java
+++ src/main/java/Model/DatabaseInteraction/ProfileDAO.java
@@ -4,6 +4,8 @@ package Model.DatabaseInteraction;
 import Model.Profile.UserProfile;
 
 import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ProfileDAO {
     private Connection con = null;
@@ -71,6 +73,38 @@ public class ProfileDAO {
 
         return profileId;
 
+    }
+
+    public List<UserProfile> getProfileList(){
+        List<UserProfile> profiles = new LinkedList<UserProfile>();
+        String sql = "SELECT * FROM fitnessjournal.account;";
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int profileId = resultSet.getInt("account_id");
+                String fName = resultSet.getString("first_name");
+                String lName = resultSet.getString("last_name");
+                int age = resultSet.getInt("age");
+                String gender = resultSet.getString("gender");
+                double height = resultSet.getDouble("height");
+                double weight = resultSet.getDouble("weight");
+                String specialPeriod = resultSet.getString("special_period");
+                Boolean hasWeightScale = resultSet.getInt("has_weight_scale") == 1;
+                UserProfile profile = new UserProfile(fName,lName,age,gender,height,weight,
+                        specialPeriod,hasWeightScale);
+                profile.setId(profileId);
+                profiles.add(profile);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(String.format("get profile list success: "+profiles));
+        return profiles;
     }
 
 
